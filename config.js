818f198e1cbf1909e653b167c48757f99f9d6337
minres22 <minres22@gmail.com>
2026-08-24 20:20:41 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-window.AQUENZA_CONFIG={SUPABASE_URL:"https://agrymhbsflshjcrzpksj.supabase.co/rest/v1/",SUPABASE_ANON_KEY:"agrymhbsflshjcrzpksj"};
+window.AQUENZA_CONFIG={SUPABASE_URL:"https://agrymhbsflshjcrzpksj.supabase.co",SUPABASE_ANON_KEY:"sb_publishable_lhtJgMF0lf1MAXcd4DjIVQ_cl8GW5eS"};
